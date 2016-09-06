4c3d2e70304147e098dbaef12566c893ad319f2d
Maria Blair <maria.klachko@gmail.com>
2016-09-06 10:16:20 -0700

    Added Version 8 of the app. Refactored refactored our code by adding 'handlers' object to keep all our handler functions and by adding inputs on display which will capture the user's input and connect JS to it.

---
+++ Version8 - Inputs/script8.js
@@ -0,0 +1,115 @@
+var todoList = {
+  todos: [],
+
+  // we added loop to display all todoText in our array:
+  displayTodos: function() {
+    // we introduced logic:
+    if (this.todos.length === 0) {
+      console.log('Your todo list is empty!');
+    } else {
+      console.log("My Todos: ");
+      for (var i = 0; i < this.todos.length; i++) {
+        if (this.todos[i].completed === true) {
+          console.log('(X)', this.todos[i].todoText);
+        } else {
+          console.log('( )', this.todos[i].todoText);
+        }
+      }
+    }
+  },
+
+  addTodo: function(todoText) {
+    this.todos.push({
+      todoText: todoText,
+      completed: false
+    });
+    this.displayTodos();
+  },
+
+  changeTodo: function(position, todoText) {
+    this.todos[position].todoText = todoText;
+    this.displayTodos();
+  },
+
+  deleteTodo: function(position) {
+    this.todos.splice(position, 1);
+    this.displayTodos();
+  },
+
+  toggleCompleted: function(position) {
+    var todo = this.todos[position];
+    todo.completed = !todo.completed;
+    this.displayTodos();
+  },
+
+  toggleAll: function() {
+
+    // Case 1: if all true, make them all false:
+    // # of completed todos = # of all todos
+
+    // Since we need to compare them, let's create vars:
+    var allTodos = this.todos.length;
+    var completedTodos = 0;
+
+    // Get # of completed todos:
+    for (var i = 0; i < allTodos; i++) {
+      if (this.todos[i].completed === true) {
+        completedTodos++;
+      }
+    }
+
+    // Case 1: if everything is true, make them all false:
+    if (completedTodos === allTodos) {
+      // make everthing false:
+      for (var i = 0; i < allTodos; i++) {
+        this.todos[i].completed = false;
+      }
+      // this.displayTodos(); -- we don't need display for every case - just in the end
+    } else {
+
+
+    // Case 2:  If some are true, make them all true:
+
+      // even if only 1 is true, so we need to find the 1st true:
+      for (var k = 0; k < allTodos; k++) {
+        this.todos[k].completed = true;
+      }
+    }
+    // after the case logic, it will display the outcome:
+    this.displayTodos();
+  }
+
+};
+
+// *** Handlers ***
+
+var handlers = {
+  displayTodos: function() {
+    todoList.displayTodos();
+  },
+  addTodo: function() {
+    var addTodoInputText = document.getElementById('addTodoInputText');
+    todoList.addTodo(addTodoInputText.value);
+    addTodoInputText.value = '';
+  },
+  changeTodo: function() {
+    var changeTodoPositionInput = document.getElementById('changeTodoPositionInput');
+    var changeTodoTextInput = document.getElementById('changeTodoTextInput');
+    todoList.changeTodo(changeTodoPositionInput.valueAsNumber, changeTodoTextInput.value);
+    changeTodoPositionInput.value = '';
+    changeTodoTextInput.value = '';
+  },
+  deleteTodo: function() {
+    var deleteTodoPositionInput = document.getElementById('deleteTodoPositionInput');
+    todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber, 1);
+    deleteTodoPositionInput.value = '';
+  },
+  toggleCompleted: function() {
+    var toggleCompletedPositionInput = document.getElementById('toggleCompletedPositionInput');
+    todoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
+    toggleCompletedPositionInput.value = '';
+  },
+  toggleAll: function() {
+    todoList.toggleAll();
+  },
+};
